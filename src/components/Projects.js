4308d6b1a25c9a09bdf7bceccb05570aef312c39
NaveenChandar <12165092+navchandar@users.noreply.github.com>
2025-07-15 16:58:32 +0530

    Minor updates to styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,45 +2,45 @@ import React from 'react';
 import './Projects.css';
 
 const projects = [
-  {
-    title: 'Daily Naukri Update',
-    description: 'Automatically update your Naukri profiles daily using Selenium script',
-    link: 'https://github.com/navchandar/Naukri/',
-  },
-  {
-    title: 'Look Like Scanned',
-    description: 'Python based CLI to turn any image/ PDF into a “scanned” version',
-    link: 'https://navchandar.github.io/look-like-scanned/',
-  },
-  {
-    title: 'Civic Media Scout',
-    description: 'An Open Source government department contact information for a more connected democracy',
-    link: 'https://navchandar.github.io/civic-media-scout/',
-  },
-  {
-    title: 'Random Name Generator',
-    description: 'Python data provider module. Useful for testing with random data',
-    link: 'https://github.com/navchandar/Python-Random-Name-Generator/',
-  },
+    {
+        title: 'Daily Naukri Update',
+        description: 'Automatically update your Naukri profiles daily using Selenium script',
+        link: 'https://github.com/navchandar/Naukri/',
+    },
+    {
+        title: 'Look Like Scanned',
+        description: 'Python based CLI to turn any image/ PDF into a “scanned” version',
+        link: 'https://navchandar.github.io/look-like-scanned/',
+    },
+    {
+        title: 'Civic Media Scout',
+        description: 'An Open Source government department contact information for a more connected democracy',
+        link: 'https://navchandar.github.io/civic-media-scout/',
+    },
+    {
+        title: 'Random Name Generator',
+        description: 'Python data provider module. Useful for testing with random data',
+        link: 'https://github.com/navchandar/Python-Random-Name-Generator/',
+    },
 ];
 
+
 function Projects() {
-  return (
-    <div className="projects">
-      <h3>My Projects</h3>
-      <div className="project-list">
-        {projects.map((project, index) => (
-          <div className="project-card" key={index}>
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">
-              View Project
-            </a>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="projects">
+            <h3>My Projects</h3>
+            <div className="project-list">
+                {projects.map((project, index) => (
+                    <div className="project-card" key={index}>
+                        <h3>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer">{project.title}</a>
+                        </h3>
+                        <p>{project.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Projects;
