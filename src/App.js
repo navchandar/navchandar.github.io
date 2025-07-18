261952d7069c3f4eb0150f71fd7dd68dee9cfc99
NaveenChandar <12165092+navchandar@users.noreply.github.com>
2025-07-19 00:19:36 +0530

    Minor style updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App fade-in">
       <Header/>
       <Subtitle/>
       
