3fbbc24c034b951163c3a561731e47e03dc963f4
NaveenChandar <12165092+navchandar@users.noreply.github.com>
2025-07-20 23:19:06 +0530

    Adding footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import './Footer.css'
 
 const Footer = () => {
   const [showTitle, setShowTitle] = useState(false);
+  const [scale, setScale] = useState(1);
 
 
   useEffect(() => {
@@ -11,12 +12,23 @@ const Footer = () => {
       const scrollY = window.scrollY;
       const docHeight = document.documentElement.scrollHeight;
       const winHeight = window.innerHeight;
+      const scrollBottom = scrollY + winHeight;
+      const distanceFromBottom = docHeight - scrollBottom;
 
-      if (scrollY + winHeight >= docHeight - 10) {
+      // Display socials title when user nears bottom 
+      if (scrollBottom >= docHeight - 10) {
         setShowTitle(true);
       } else {
         setShowTitle(false);
       }
+
+      // Make icons big as user nears bottom (0 to 1.5x)
+      const newScale = distanceFromBottom < 300
+        ? 1 + (1 - distanceFromBottom / 300) * 0.5
+        : 1;
+
+      setScale(newScale);
+
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -27,8 +39,8 @@ const Footer = () => {
   return (
     <div id="footer">
       <h3 className={`footer-title ${showTitle ? 'visible' : ''}`}>My Socials</h3>
-      
-      <div className="social-links">
+
+      <div className="social-links" style={{ transform: `scale(${scale})` }} >
         <a href="https://www.linkedin.com/in/naveenchandar/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile" title="LinkedIn Profile">
           <svg className="social-icon" height="72" width="72" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z" fill="currentColor" /><path d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z" fill="#FFF" /></g></svg>
         </a>
